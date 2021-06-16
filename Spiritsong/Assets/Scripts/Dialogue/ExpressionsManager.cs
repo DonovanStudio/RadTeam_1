98fc5778ac8662b9a99757d901cc10651662085c
seamsly <shaylahan@yahoo.ca>
2021-06-16 16:43:19 -0400

    Created Yarn commands for sprite animations

--- Spiritsong/Assets/Scripts/Dialogue/ExpressionsManager.cs
+++ Spiritsong/Assets/Scripts/Dialogue/ExpressionsManager.cs
@@ -9,11 +9,14 @@ public class ExpressionsManager : MonoBehaviour
     /* Attached this script to an image of a character on the UI. */
 
     public Sprite[] characterSprites; // 0 = Neutral, 1 = Happy, 2 = Angry, 3 = Sad, etc. (add more as we come up with them)
+    public AnimationClip[] animationClips;
+    Animator anim;
     Image currentExpression;
 
     private void Awake()
     {
         currentExpression = this.GetComponent<Image>();
+        anim = this.GetComponent<Animator>();
     }
 
     [YarnCommand("neutral")]
@@ -38,5 +41,40 @@ public class ExpressionsManager : MonoBehaviour
     public void SetExpressionSad()
     {
         currentExpression.sprite = characterSprites[3];
+    }
+
+    [YarnCommand("turnL")]
+    public void TurnLeft()
+    {
+        anim.SetBool("Turn Right", false);
+        anim.SetBool("Facing Right", false);
+        anim.SetBool("Turn Left", true);
+        anim.SetBool("Hopping", false);
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    [YarnCommand("turnR")]
+    public void TurnRight()
+    {
+        anim.SetBool("Turn Right", true);
+        anim.SetBool("Facing Right", true);
+        anim.SetBool("Turn Left", false);
+        anim.SetBool("Hopping", false);
+        transform.rotation = new Quaternion (0, 180, 0, 0);
+    }
+
+    [YarnCommand("hop")]
+    public void Hop()
+    {
+        anim.SetBool("Turn Right", false);
+        anim.SetBool("Turn Left", false);
+        anim.SetBool("Hopping", true);
+    }
+
+    public void PauseClip()
+    {
+        anim.SetBool("Turn Right", false);
+        anim.SetBool("Turn Left", false);
+        anim.SetBool("Hopping", false);
     }
 }
