896b776d805fe001232e5779819181fbc0c5aca2
seamsly <seamus.ly@outlook.com>
2021-07-07 00:07:23 -0400

    Added popup container and text variation

--- Spiritsong/Assets/Scripts/Dialogue/HintsManager.cs
+++ Spiritsong/Assets/Scripts/Dialogue/HintsManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 using UnityEngine.UI;
+using TMPro;
 
 public class HintsManager : MonoBehaviour
 {
     AbilityVariableStorage abilityVar;
     [SerializeField] Animator notificationAnimator;
     [SerializeField] Image popupSprite;
+    [SerializeField] TextMeshProUGUI popupText;
     [SerializeField] Sprite[] characterSprites;
 
     // Start is called before the first frame update
@@ -24,6 +26,7 @@ public class HintsManager : MonoBehaviour
         {
             abilityVar.hintsAvailable[0] = true;
             popupSprite.sprite = characterSprites[0];
+            popupText.text = "I'm here if you need aid!";
             notificationAnimator.SetBool("HintAvailable", true);
             StartCoroutine(HideNotifs());
         }
@@ -32,6 +35,7 @@ public class HintsManager : MonoBehaviour
         {
             abilityVar.hintsAvailable[1] = true;
             popupSprite.sprite = characterSprites[1];
+            popupText.text = "Hey, I got a hint for ya!";
             notificationAnimator.SetBool("HintAvailable", true);
             StartCoroutine(HideNotifs());
         }
@@ -40,6 +44,7 @@ public class HintsManager : MonoBehaviour
         {
             abilityVar.hintsAvailable[2] = true;
             popupSprite.sprite = characterSprites[2];
+            popupText.text = "I might be able to help here!";
             notificationAnimator.SetBool("HintAvailable", true);
             StartCoroutine(HideNotifs());
         }
